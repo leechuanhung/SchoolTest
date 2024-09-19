0370bbbbdb43dd6142a96d47c9deefc8e2f94d7a
leechuanhung <163072144+leechuanhung@users.noreply.github.com>
2024-09-19 13:30:42 +0900

    점프 애니메이션(미완)

--- Assets/2.Script/Player.cs
+++ Assets/2.Script/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float jumpForce = 7f;       // 점프 힘
+    public float jumpForce = 5f;       // 점프 힘
     public float maxSpeed = 6f;
     private bool isGrounded = true;    // 플레이어가 땅에 닿아 있는지 확인
     public float Speed = 5f;    //움직이는 힘
@@ -26,13 +26,9 @@ public class Player : MonoBehaviour
     {
 
 
-        /*if (Input.GetKeyDown(KeyCode.Z) && isGrounded) //Jump
-        {
-            Jump();
-        }
 
         //max Speed
-        if(rb.velocity.x > maxSpeed) //Right Max Speed
+        /*if(rb.velocity.x > maxSpeed) //Right Max Speed
             rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
         else if(rb.velocity.x < maxSpeed*(-1)) //Left Max Speed
             rb.velocity = new Vector2(maxSpeed*(-1), rb.velocity.y);*/
@@ -40,6 +36,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        bool isJumping = !isGrounded && rb.velocity.y > 0;
 
         //Move Speed
         if (Input.GetKey(KeyCode.RightArrow))
@@ -70,10 +67,23 @@ public class Player : MonoBehaviour
             SpriteRenderer.flipX = true;
         if (Input.GetKey(KeyCode.RightArrow))
             SpriteRenderer.flipX = false;
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Z) && isGrounded) //Jump
+        {
+            Jump();
+            anim.SetTrigger("Jump");
+            //anim.SetBool("isJump", !isGrounded);
+        }
+        /*if(Input.GetKeyDown(KeyCode.Z))
+        {
+            transform.Translate(Vector2.up * jumpForce);
+            anim.SetTrigger("Jump");
+        }*/
     }
 
     //땅에 닿았는지 확인하기 위해 충돌 처리
-    /*private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
         {
@@ -84,7 +94,7 @@ public class Player : MonoBehaviour
     {
         //Rigidbody에 위로 힘을 가해 점프 처리
         rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-        //isGrounded = false; //점프 후에는 땅에 있지 않음
-    }*/
+        isGrounded = false; //점프 후에는 땅에 있지 않음
+    }
 
 }
